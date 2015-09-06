19c89e9758f6c0eb47d226d9e28a3956a997fc29
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2015-09-06 16:50:38 +0200

    Remove schedule-info.js from repo

--- master/src/schedule/schedule-info.js
+++
@@ -1,4 +0,0 @@
-moreaEventData = [
-  {title: "Experience the Baz", url: "/e-learning-2015gh-pages/morea/baz/experience.html", start: "2015-06-11T18:00"},
-  {title: "Learn to Baz", url: "/e-learning-2015gh-pages/modules/baz", start: "2015-06-11", end: "2015-06-15"}
-]
