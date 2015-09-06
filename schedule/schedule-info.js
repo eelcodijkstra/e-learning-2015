11593ec15fc0b5fce4acfb04945cab6e911d38ce
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2015-09-06 16:16:10 +0200

    First commit +++

--- schedule/schedule-info.js
+++ schedule/schedule-info.js
@@ -1,4 +1,4 @@
 moreaEventData = [
-  {title: "Experience the Baz", url: "/e-learning-2015gh-pages/morea/baz/experience.html", start: "2015-06-11T18:00"},
-  {title: "Learn to Baz", url: "/e-learning-2015gh-pages/modules/baz", start: "2015-06-11", end: "2015-06-15"}
+  {title: "Experience the Baz", url: "/e-learning-2015/gh-pages/morea/baz/experience.html", start: "2015-06-11T18:00"},
+  {title: "Learn to Baz", url: "/e-learning-2015/gh-pages/modules/baz", start: "2015-06-11", end: "2015-06-15"}
 ]
